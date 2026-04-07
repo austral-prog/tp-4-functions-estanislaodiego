269e5ce1b26f9a160243a2883f0f6fd101e7d90d
estanislaodiego <ediego@mail.austral.edu.ar>
2026-04-07 11:52:18 -0300

    Update text_analyzer.py

--- text_analyzer.py
+++ text_analyzer.py
@@ -23,7 +23,9 @@ def total_letters(text):
     Retorna la cantidad total de letras en el texto.
     Debe USAR las funciones count_vowels y count_consonants.
     """
-    return "ANSWER HERE"  # Remove this line and implement
+
+    return (count_vowels(text) + count_consonants(text))
+
 
 def vowel_percentage(text):
     """
@@ -33,7 +35,12 @@ def vowel_percentage(text):
 
     Ejemplo: "hola" tiene 2 vocales de 4 letras → 50.0
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    if total_letters(text) > 0:
+        return round((count_vowels(text) / total_letters(text) *100),1)
+
+    else:
+        return 0.0
+
 
 def analyze_text(text):
     """
@@ -44,4 +51,8 @@ def analyze_text(text):
 
     Ejemplo: analyze_text("hola") → "V:2 C:2 T:4 P:50.0%"
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    v = count_vowels(text)
+    c = count_consonants(text)
+    t = total_letters(text)
+    p = vowel_percentage(text)
+    return f"V:{v} C:{c} T:{t} P:{p}%"
